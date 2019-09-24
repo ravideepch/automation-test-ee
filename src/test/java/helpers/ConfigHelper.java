bc68c896cc43e11f2bfa16502038d8db3bdde714
Ravi Cherukuri <rcherukuri@financialforce.com>
2019-09-24 19:25:02 +0100

    implemented multibrowser with maven - property -Dbrowser

--- src/test/java/helpers/ConfigHelper.java
+++ src/test/java/helpers/ConfigHelper.java
@@ -2,6 +2,7 @@ package helpers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,10 +10,22 @@ public class ConfigHelper
 {
 	public static WebDriver setupBrowser()
 	{
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = createWebDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
 		return driver;
 	}
+
+	public static WebDriver createWebDriver() {
+        String webdriver = System.getProperty("browser", "chrome");
+
+        if (webdriver.equalsIgnoreCase("firefox"))
+        	return new FirefoxDriver();
+        else if (webdriver.equalsIgnoreCase("chrome"))
+        	return new ChromeDriver();
+        else
+        	throw new RuntimeException("Unsupported webdriver: " + webdriver);
+
+    }
 }
